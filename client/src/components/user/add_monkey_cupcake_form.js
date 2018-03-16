02bf50051686112d2dfaa2ee1686f7cf01e8ff2b
Jess <jessachandler@gmail.com>
2018-03-16 12:43:37 +0900

    can render the field on add cupcake but adds to all monkey cards

--- client/src/components/user/add_monkey_cupcake_form.js
+++ client/src/components/user/add_monkey_cupcake_form.js
@@ -6,7 +6,17 @@ import renderTextField from '../helpers/form_helpers';
 
 class AddMonkeyCupcakeForm extends Component {
 
+    cancel(e) {
+    e.preventDefault()
+
+    if (this.props.onCancel) {
+      this.props.onCancel()
+    }
+  }
+
     render() {
+        if (this.props.isVisible === false)
+     return null
         const { handleSubmit } = this.props;
 
         return (
@@ -26,11 +36,21 @@ class AddMonkeyCupcakeForm extends Component {
                         Add Cupcake
                         <i className="material-icons right">done</i>
                     </button>
+                    <button
+                        className="btn-large danger"
+                        type="cancel"
+                        onClick={e => this.cancel(e)}
+                    >
+                        Cancel
+                        <i className="material-icons right">times</i>
+                    </button>
                 </form>
             </div>
         );
     }
 }
+
+
 
 const validate = values => {
     const errors = {};
--- client/src/components/user/monkeys.js
+++ client/src/components/user/monkeys.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import AddMonkeyCupcakeForm from './add_monkey_cupcake_form';
+
 export default class Monkeys extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { cupcakeFieldVisible: false };
+    }
+
+    showCupcakeField() {
+        this.setState({ cupcakeFieldVisible: true });
+    }
+    hideCupcakeField() {
+        this.setState({ cupcakeFieldVisible: false });
+    }
+
+    handleCupcakeSubmit({ monkeyid, color }) {
+        console.log('handleSubmitCupcake with', monkeyid, color);
+        const userid = this.props.user._id;
+        this.props.createCupcake({ userid, monkeyid, color });
+        this.onSaveCupcake()
+    }
+
+    onSaveCupcake() {
+        console.log('save cupake');
+
+        this.hideCupcakeField();
+    }
+
+    onCancelCupcake() {
+        console.log('cancel cupcake');
+        this.hideCupcakeField();
+    }
+
     renderMonkeyCupcakes(mid) {
         if (
             this.props.cupcakes !== undefined &&
@@ -15,7 +47,11 @@ export default class Monkeys extends Component {
             return (
                 <div className="row">
                     {mc.map((cupcake, i) => (
-                        <a className="col s4" key={i} href={`#${cupcake.monkey}${cupcake.color}`}>
+                        <a
+                            className="col s4"
+                            key={i}
+                            href={`#${cupcake.monkey}${cupcake.color}`}
+                        >
                             {cupcake.color}
                         </a>
                     ))}
@@ -25,25 +61,31 @@ export default class Monkeys extends Component {
             console.log('Monkeys has no cupcakes');
         }
     }
+
     render() {
         console.log('Monkeys has:', this.props.monkeys);
         return (
             <div>
                 {this.props.monkeys.map((monkey, i) => (
                     <div key={i} className="col s12 m6 l4">
-                    <div className="card blue-grey darken-1 ">
-                        <div className="card-content white-text">
-                            <span className="card-title">{monkey.name}</span>
-                            This monkey has cupcakes:
-                            {this.renderMonkeyCupcakes(monkey._id)}
-                        </div>
+                        <div className="card blue-grey darken-1 ">
+                            <div className="card-content white-text">
+                                <span className="card-title">
+                                    {monkey.name}
+                                </span>
+                                This monkey has cupcakes:
+                                {this.renderMonkeyCupcakes(monkey._id)}
+                            </div>
+                            <AddMonkeyCupcakeForm
+                                isVisible={this.state.cupcakeFieldVisible}
+                                onSubmit={this.handleCupcakeSubmit}
+                                onCancel={() => this.onCancelCupcake()}
+                            />
 
-                        <div className="card-action">
-                                <a href="#">
-                                    New Cupcake
-                                </a>
+                            <div className="card-action">
+                                <a href="#" onClick={()=>this.showCupcakeField()}>New Cupcake</a>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 ))}
             </div>
